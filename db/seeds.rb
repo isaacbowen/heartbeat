eaa651a49c12f206ba60e599a22ef3802afdb767
Isaac Bowen <ikebowen@gmail.com>
2014-05-27 18:16:58 -0500

    seed with our metrics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+metrics = [
+  ['Community', 'Build team unity and alignment by consistently connecting, sharing, and supporting.'],
+  ['Shared Ownership', 'Practice commitment and accountability as individuals and groups, in service of a shared mission.'],
+  ['Craftsmanship', 'Be constantly deliberate in the quality of your product, of your environment, and of your community.'],
+  ['Communication and Awareness', 'Proactively share and seek state, horizontally and vertically, to build solidarity and trust.'],
+  ['Balanced Pragmatism', 'Weigh quality versus resources, knowing when to force an exception.'],
+]
+
+metrics.each_with_index do |args, i|
+  name, description = args
+  Metric.where(name: name).first_or_create(order: i, description: description)
+end
