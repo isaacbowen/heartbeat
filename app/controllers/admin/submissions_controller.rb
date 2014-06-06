fc4109326b38a1433d67c777e2b5ac6ff35802d4
Isaac Bowen <ikebowen@gmail.com>
2014-06-06 09:13:52 -0500

    mea culpa

--- app/controllers/admin/submissions_controller.rb
+++ app/controllers/admin/submissions_controller.rb
@@ -6,7 +6,7 @@ class Admin::SubmissionsController < Admin::BaseController
   def batch
     User.all.each do |user|
       submission = user.submissions.create!
-      UserMailer.submission_created(submission, params[:subject], params[:message]).deliver
+      UserMailer.submission_created(submission, params[:subject], params[:message], params[:from]).deliver
     end
 
     flash.notice = "#{User.count} submission request(s) sent"
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,14 +2,14 @@ class UserMailer < ActionMailer::Base
 
   default from: 'ibowen@enova.com'
 
-  def submission_created submission, subject = nil, message = nil
+  def submission_created submission, subject = nil, message = nil, from = nil
     @subject = subject.presence || 'Invitation to Heartbeat'
     @message = message.presence || 'We have a message for you!'
     @user = submission.user
     @submission = submission
     @domain = ENV['HEARTBEAT_DOMAIN']
 
-    mail to: @user.email, subject: @subject, from: @from
+    mail to: @user.email, subject: @subject, from: from
   end
 
 end
