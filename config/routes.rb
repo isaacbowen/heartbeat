e03c9b2e367839d6b7670af760d0555727f75145
Isaac Bowen <ikebowen@gmail.com>
2014-07-17 12:45:56 -0500

    add an about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'meta#root'
+  get 'about' => 'meta#about'
 
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   get 'login' => redirect('/users/auth/google_oauth2')
