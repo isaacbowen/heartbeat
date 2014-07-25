219f6d5b67a03374b060f09a31eb9b6bb2e1bb82
Isaac Bowen <ikebowen@gmail.com>
2014-07-25 07:08:15 -0500

    dalli, memcachecloud

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+  if ENV['MEMCACHEDCLOUD_SERVERS']
+    config.cache_store = :dalli_store, ENV['MEMCACHEDCLOUD_SERVERS'].split(','), { :username => ENV['MEMCACHEDCLOUD_USERNAME'], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
